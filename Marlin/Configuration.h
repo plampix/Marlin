05d6e316d53da2acdd964d771c535348d4e33785
Peter Palm <peterp@bahamut.watdoejefout.nl>
2024-06-16 17:17:19 +0200

    use I/O pin for probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1346,7 +1346,7 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
 #define USE_PROBE_FOR_Z_HOMING
@@ -1364,7 +1364,7 @@
  *    - Normally-closed (NC) also connect to GND.
  *    - Normally-open (NO) also connect to 5V.
  */
-//#define Z_MIN_PROBE_PIN 29
+#define Z_MIN_PROBE_PIN PD5
 
 /**
  * Probe Type
