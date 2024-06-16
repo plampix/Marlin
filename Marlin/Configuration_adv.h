bcd83c684a597d25e990972df108e09e42e524de
Peter Palm <peterp@bahamut.watdoejefout.nl>
2024-06-17 01:59:45 +0200

    Increase buffer sizes some more

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2599,7 +2599,7 @@
 #if ALL(HAS_MEDIA, DIRECT_STEPPING)
   #define BLOCK_BUFFER_SIZE  8
 #elif HAS_MEDIA
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE 32
 #else
   #define BLOCK_BUFFER_SIZE 32
 #endif
@@ -2617,7 +2617,7 @@
 // For debug-echo: 128 bytes for the optimal speed.
 // Other output doesn't need to be that speedy.
 // :[0, 2, 4, 8, 16, 32, 64, 128, 256]
-#define TX_BUFFER_SIZE 32
+#define TX_BUFFER_SIZE 128
 
 // Host Receive Buffer Size
 // Without XON/XOFF flow control (see SERIAL_XON_XOFF below) 32 bytes should be enough.
