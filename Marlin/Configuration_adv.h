5c8b4f082b918ba669396721413bdb2e52d275cd
Peter Palm <peterp@bahamut.watdoejefout.nl>
2024-06-19 00:36:43 +0200

    change stealthchop threshold

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3357,11 +3357,11 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     120  // [mm/s]
+  #define X_HYBRID_THRESHOLD     180  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     120
+  #define Y_HYBRID_THRESHOLD     180
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD      20
+  #define Z_HYBRID_THRESHOLD     100
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
