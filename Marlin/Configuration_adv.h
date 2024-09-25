acc7fba871555090c67bc6243e9110678536ade0
Peter Palm <peterp@stack.nl>
2024-09-25 11:22:39 +0200

    IS

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1205,11 +1205,11 @@
 //#define INPUT_SHAPING_Z
 #if ANY(INPUT_SHAPING_X, INPUT_SHAPING_Y, INPUT_SHAPING_Z)
   #if ENABLED(INPUT_SHAPING_X)
-    #define SHAPING_FREQ_X  80          // (Hz) The default dominant resonant frequency on the X axis.
+    #define SHAPING_FREQ_X  45          // (Hz) The default dominant resonant frequency on the X axis.
     #define SHAPING_ZETA_X  0.15        // Damping ratio of the X axis (range: 0.0 = no damping to 1.0 = critical damping).
   #endif
   #if ENABLED(INPUT_SHAPING_Y)
-    #define SHAPING_FREQ_Y  35          // (Hz) The default dominant resonant frequency on the Y axis.
+    #define SHAPING_FREQ_Y  23          // (Hz) The default dominant resonant frequency on the Y axis.
     #define SHAPING_ZETA_Y  0.15        // Damping ratio of the Y axis (range: 0.0 = no damping to 1.0 = critical damping).
   #endif
   #if ENABLED(INPUT_SHAPING_Z)
