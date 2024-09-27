6b7e171cf8d5560c855b606a37665caa4f0fc911
Peter Palm <peterp@stack.nl>
2024-09-27 11:22:48 +0200

    saner defaults

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1264,11 +1264,11 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 1000, 1000, 30, 12 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 400, 30, 12 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 2000, 2000, 100, 50 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 50, 150 } // ...or, set your own edit limits
 #endif
 
 /**
